fc112558f67e9c2f815d289aa43d17de1c6b3c45
bongovi <mgovindraj@gmail.com>
2024-11-28 16:12:41 -0800

    While and For loop

--- Calculator.py
+++ Calculator.py
@@ -12,8 +12,17 @@
 
 # print(f"{z:,}")
 
-x = float(input("What's x? "))
-y = float(input("What's y? "))
-z = x/y
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+# z = x/y
 
-print(f"{z:.2f}")
+# print(f"{z:.2f}")
+
+def main():
+    x = int(input("What's x? "))
+    print("X squared is ", square(x))
+
+def square(n):
+    return pow(n,2)
+
+main()
--- Hello.py
+++ Hello.py
@@ -1,4 +1,16 @@
-name = input("WHat's your name?")
-name = name.strip().title()
-first,last = name.split(" ")
-print(f"Hello, {last}")
+# name = input("WHat's your name?")
+# name = name.strip().title()
+# first,last = name.split(" ")
+# print(f"Hello, {last}")
+
+
+def main():
+    name = input("What's your name? ")
+    hello(name)
+    hello()
+
+def hello(to="BonGovi"):
+    print(f"Hello, {to}")
+
+main()
+
